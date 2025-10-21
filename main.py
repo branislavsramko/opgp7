1313a1fff3257318e8c7908800917bcfc0fd1d54
Branislav Sramko <branislavsramko1@gmail.com>
2025-10-21 09:13:56 +0200

    Modul kruh

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from tvary import stvorec
 from tvary import obdlznik
+import tvary.kruh
 print(f"Obvod štvorca o veľkosti 10 je {stvorec.obvod(10)}")
 print(f"Obvod obdĺžnika o veľkosti 10 x 15 je {obdlznik.obvod(10, 15)}")
+print(f"Obvod kruhu o veľkosti 10 je {tvary.kruh.obvod(10)}")
+
 
 print("hello world")
